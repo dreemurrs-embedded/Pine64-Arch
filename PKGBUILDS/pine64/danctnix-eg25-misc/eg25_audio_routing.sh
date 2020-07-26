972a54bdd86b98b05929e4d407238fe37381e3ee
Danct12 <danct12@disroot.org>
2020-07-27 03:25:07 +0700

    pkg: pine64: danctnix-eg25-misc: enable volte
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/pine64/danctnix-eg25-misc/eg25_audio_routing.sh
+++ PKGBUILDS/pine64/danctnix-eg25-misc/eg25_audio_routing.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while [ ! -e /dev/EG25.AT ]; do sleep 1 ; done
-sleep 4
+sleep 10
 
 # Set modem audio configs
 if echo "AT+QDAI=1,0,0,2,0,1,1,1" | atinout - /dev/EG25.AT - | grep -q OK; then
@@ -14,4 +14,18 @@ if echo 'AT+QCFG="risignaltype","physical"' | atinout - /dev/EG25.AT - | grep -q
         echo "Successfully configured modem ring wakeup"
 else
         echo "Failed to set modem ring wakeup: $?"
+fi
+
+# Setup VoLTE
+if echo "AT+QMBNCFG=\"AutoSel\",1" | atinout - /dev/EG25.AT - | grep -q OK; then
+        echo "Successfully configured VoLTE to AutoSel"
+else
+        echo "Failed to configure VoLTE Profile: $?"
+fi
+
+# Enable VoLTE
+if echo "AT+QCFG=\"ims\",1" | atinout - /dev/EG25.AT - | grep -q OK; then
+        echo "Successfully enabled VoLTE"
+else
+        echo "Failed to enable VoLTE: $?"
 fi
