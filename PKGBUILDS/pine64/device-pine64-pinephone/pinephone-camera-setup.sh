20aff73d2d0858af98df45e0c6b2aba16e91e327
Danct12 <danct12@disroot.org>
2020-07-28 02:20:22 +0700

    pkg: pine64: device-pine64-pinephone: crash fix BH and cam fix
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/pine64/device-pine64-pinephone/pinephone-camera-setup.sh
+++ PKGBUILDS/pine64/device-pine64-pinephone/pinephone-camera-setup.sh
@@ -19,15 +19,24 @@ if [ ! -c "$CAMERA_DEV" ]; then
     exit 1
 fi
 
+for dir in /sys/class/video4linux/v4l-subdev*; do
+    if grep -q "ov5640" $dir/name; then
+        BACK_CAMERA=`cat $dir/name`
+    elif grep -q "gc2145" $dir/name; then
+        FRONT_CAMERA=`cat $dir/name`
+    fi
+done
+
 # Configure camera
-media-ctl -d $CAMERA_DEV --set-v4l2 '"ov5640 3-004c":0[fmt:YUYV8_2X8/1280x720]'
-media-ctl -d $CAMERA_DEV --set-v4l2 '"gc2145 3-003c":0[fmt:YUYV8_2X8/1280x720]'
+media-ctl -d $CAMERA_DEV --set-v4l2 "\"$BACK_CAMERA\":0[fmt:YUYV8_2X8/1280x720]"
+media-ctl -d $CAMERA_DEV --set-v4l2 "\"$FRONT_CAMERA\":0[fmt:YUYV8_2X8/1280x720]"
 
 # Select camera
 if [ "$1" = "front" ]; then
-    media-ctl -d $CAMERA_DEV --links '"ov5640 3-004c":0->"sun6i-csi":0[0]'
-    media-ctl -d $CAMERA_DEV --links '"gc2145 3-003c":0->"sun6i-csi":0[1]'
+    media-ctl -d $CAMERA_DEV --links "\"$BACK_CAMERA\":0->\"sun6i-csi\":0[0]"
+    media-ctl -d $CAMERA_DEV --links "\"$FRONT_CAMERA\":0->\"sun6i-csi\":0[1]"
 else
-    media-ctl -d $CAMERA_DEV --links '"gc2145 3-003c":0->"sun6i-csi":0[0]'
-    media-ctl -d $CAMERA_DEV --links '"ov5640 3-004c":0->"sun6i-csi":0[1]'
+    media-ctl -d $CAMERA_DEV --links "\"$FRONT_CAMERA\":0->\"sun6i-csi\":0[0]"
+    media-ctl -d $CAMERA_DEV --links "\"$BACK_CAMERA\":0->\"sun6i-csi\":0[1]"
 fi
+
--- PKGBUILDS/pine64/device-pine64-pinephone/pinephone-suspend-hook.sh
+++ PKGBUILDS/pine64/device-pine64-pinephone/pinephone-suspend-hook.sh
@@ -1,12 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 # Copyright 2020 - Dreemurrs Embedded Labs / DanctNIX Community
 #
 # This suspend hook is intended as a workaround for Bluetooth suspend oops:
 # https://gitlab.com/pine64-org/linux/-/issues/24
 
+# Uncomment this (or add to profile.d) if you want to suspend properly on Braveheart (disabled due to dmesg spam)
+#IT_NEVER_CAME_BACK_ON=1
+
+# Set it to non-empty once it finds the magic string
+grep -rq IT_NEVER_CAME_BACK_ON /etc/profile.d && IT_NEVER_CAME_BACK_ON=1
+
 prepare_suspend() {
 	echo "Preparing to suspend..."
+
+	# Braveheart users need this if they want to suspend properly
+	[ -n "$IT_NEVER_CAME_BACK_ON" ] && echo "1c19000.usb" > /sys/bus/platform/drivers/musb-sunxi/unbind
+
 	echo serial0-0 > /sys/bus/serial/drivers/hci_uart_h5/unbind
 }
 
@@ -14,6 +24,10 @@ resume_all() {
 	echo "Resuming the device..."
 	sleep 1
 	echo serial0-0 > /sys/bus/serial/drivers/hci_uart_h5/bind
+
+	# Braveheart users need this if they want to suspend properly
+	[ -n "$IT_NEVER_CAME_BACK_ON" ] && echo "1c19000.usb" > /sys/bus/platform/drivers/musb-sunxi/bind
+
 	hwclock -s
 }
 
