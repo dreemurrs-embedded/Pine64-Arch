72f484ec7747d5209ae0497fff3e6ef9a0e26282
Danct12 <danct12@disroot.org>
2020-08-05 19:13:48 +0700

    pkg: pine64: device-pine64-pinephone: faster rtc syncing
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/pine64/device-pine64-pinephone/pinephone-suspend-hook.sh
+++ PKGBUILDS/pine64/device-pine64-pinephone/pinephone-suspend-hook.sh
@@ -14,18 +14,21 @@ grep -rq IT_NEVER_CAME_BACK_ON /etc/profile.d && IT_NEVER_CAME_BACK_ON=1
 prepare_suspend() {
 	echo "Preparing to suspend..."
 
+	echo "AT+QSCLK=1" | atinout - /dev/EG25.AT -
+
 	# Braveheart users need this if they want to suspend properly
 	[ -n "$IT_NEVER_CAME_BACK_ON" ] && echo "1c19000.usb" > /sys/bus/platform/drivers/musb-sunxi/unbind
 }
 
 resume_all() {
 	echo "Resuming the device..."
+	hwclock -s
 	sleep 1
+
+	echo "AT+QSCLK=0" | atinout - /dev/EG25.AT -
 
 	# Braveheart users need this if they want to suspend properly
 	[ -n "$IT_NEVER_CAME_BACK_ON" ] && echo "1c19000.usb" > /sys/bus/platform/drivers/musb-sunxi/bind
-
-	hwclock -s
 }
 
 case $1 in
