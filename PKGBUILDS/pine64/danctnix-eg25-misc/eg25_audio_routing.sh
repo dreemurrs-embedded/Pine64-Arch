358c5723a545a5e8deca46e7b7b21b7ea622394f
Danct12 <danct12@disroot.org>
2020-08-08 21:56:11 +0700

    pkg: pine64: danctnix-eg25-misc: making sure that the modem is properly initialized
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/pine64/danctnix-eg25-misc/eg25_audio_routing.sh
+++ PKGBUILDS/pine64/danctnix-eg25-misc/eg25_audio_routing.sh
@@ -1,7 +1,39 @@
 #!/bin/sh
 
+# Copyright (C) 2020 - Dreemurrs Embedded Labs / DanctNIX Community
+# Copyright (C) 2020 - postmarketOS Contributors
+# SPDX-License-Identifier: GPL-2.0-only
+
 while [ ! -e /dev/EG25.AT ]; do sleep 1 ; done
-sleep 4
+
+# When running this script from udev the modem might not be fully initialized
+# yet, so give it some time to initialize
+#
+# We'll try to query for the firmware version for 15 seconds after which we'll
+# consider the initialization failed
+
+echo "Waiting for the modem to initialize"
+INITIALIZED=false
+for second in $(seq 1 15)
+do
+        if echo "AT+QDAI?" | atinout - /dev/EG25.AT - | grep -q OK
+        then
+                INITIALIZED=true
+                break
+        fi
+
+        echo "Waited for $second seconds..."
+
+        sleep 1
+done
+
+if $INITIALIZED
+then
+        echo "Modem initialized"
+else
+        echo "Modem failed to initialize"
+        exit 1
+fi
 
 # DTR is:     
 # - PL6/GPIO358 on BH (1.1)
