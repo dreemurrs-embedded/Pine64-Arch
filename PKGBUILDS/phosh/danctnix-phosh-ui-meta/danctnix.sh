bbd993145c949c5ffb8c689af9e756c8bc4ea737
Danct12 <danct12@disroot.org>
2020-09-10 02:05:37 +0700

    pkg: phosh: danctnix-phosh-ui-meta: add dnsmasq and custom feedbackd theme
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/phosh/danctnix-phosh-ui-meta/danctnix.sh
+++ PKGBUILDS/phosh/danctnix-phosh-ui-meta/danctnix.sh
@@ -1,3 +1,6 @@
 # Give us some room to configure things:
 export XDG_DATA_DIRS
 XDG_DATA_DIRS="/usr/share/danctnix:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
+
+# Custom feedbackd theme (mainly haptic)
+export FEEDBACK_THEME=/usr/share/feedbackd/themes/danctnix.json
