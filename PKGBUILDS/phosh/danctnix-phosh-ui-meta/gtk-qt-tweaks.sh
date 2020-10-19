d96bfb0bed9909645f108bd7052cf68f377fa133
edgar-vincent <63352906+edgar-vincent@users.noreply.github.com>
2020-10-19 14:23:08 +0200

    pkg: phosh: danctnix-phosh-ui-meta: unbreak gdk xwayland

--- PKGBUILDS/phosh/danctnix-phosh-ui-meta/gtk-qt-tweaks.sh
+++ PKGBUILDS/phosh/danctnix-phosh-ui-meta/gtk-qt-tweaks.sh
@@ -2,7 +2,7 @@
 
 # Most pure GTK3 apps use wayland by default, but some,
 # like Firefox, need the backend to be explicitely selected.
-export GDK_BACKEND=wayland
+export MOZ_ENABLE_WAYLAND=1
 
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export QT_QPA_PLATFORM=wayland
