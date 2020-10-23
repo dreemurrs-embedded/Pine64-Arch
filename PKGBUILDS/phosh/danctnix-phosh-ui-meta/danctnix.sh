54f6ffbf1e707db5c84ee1d073b906f0ff3de3ec
Danct12 <danct12@disroot.org>
2020-10-23 22:07:09 +0700

    pkg: phosh: danctnix-phosh-ui-meta: osk for 1.10.0
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/phosh/danctnix-phosh-ui-meta/danctnix.sh
+++ PKGBUILDS/phosh/danctnix-phosh-ui-meta/danctnix.sh
@@ -2,9 +2,5 @@
 export XDG_DATA_DIRS
 XDG_DATA_DIRS="/usr/share/danctnix:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
 
-# Use GLES in GDK
-export GDK_GL
-GDK_GL=gles
-
 # Custom feedbackd theme (mainly haptic)
 export FEEDBACK_THEME=/usr/share/feedbackd/themes/danctnix.json
--- PKGBUILDS/phosh/danctnix-phosh-ui-meta/gtk-qt-tweaks.sh
+++ PKGBUILDS/phosh/danctnix-phosh-ui-meta/gtk-qt-tweaks.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Use GLES in GDK
+export GDK_GL
+GDK_GL=gles
+
 # Most pure GTK3 apps use wayland by default, but some,
 # like Firefox, need the backend to be explicitely selected.
 export MOZ_ENABLE_WAYLAND=1
