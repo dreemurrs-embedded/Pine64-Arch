aefd24ac0e98160e0992fb0b16238d25d5b2b621
Danct12 <danct12@disroot.org>
2021-01-21 23:01:55 +0700

    pkg: phosh: danctnix-phosh-*: split meta and settings + some improvements
    - Show date alongside with clock, this is how Purism wants it to look like.
    - Remove feedbackd theme in favour of device-based themes
    
    Signed-off-by: Danct12 <danct12@disroot.org>

--- PKGBUILDS/phosh/danctnix-phosh-settings/danctnix.sh
+++ PKGBUILDS/phosh/danctnix-phosh-settings/danctnix.sh
@@ -1,6 +1,3 @@
 # Give us some room to configure things:
 export XDG_DATA_DIRS
 XDG_DATA_DIRS="/usr/share/danctnix:${XDG_DATA_DIRS:-/usr/local/share:/usr/share}"
-
-# Custom feedbackd theme (mainly haptic)
-export FEEDBACK_THEME=/usr/share/feedbackd/themes/danctnix.json
